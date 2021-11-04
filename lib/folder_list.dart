757ccce06d249d40dc446c70bca80bbfef1068bb
toshibo500 <walking.complex@gmail.com>
2021-11-04 22:42:10 +0900

    Add model

--- lib/folder_list.dart
+++ lib/folder_list.dart
@@ -1,7 +1,10 @@
+import 'package:flash_card/models/folder_model.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:flash_card/components/input_title_dialog.dart';
 import 'package:confirm_dialog/confirm_dialog.dart';
+import 'package:provider/provider.dart';
+import 'package:flash_card/components/input_title_dialog.dart';
+import 'package:flash_card/models/folder_list_model.dart';
 
 class FolderList extends StatefulWidget {
   const FolderList({Key? key, required this.title}) : super(key: key);
@@ -18,92 +21,127 @@ class _FolderList extends State<FolderList> {
     });
   }
 
-  final List<String> folderList = ["テキスト１", "テキスト２", "テキスト３", "テキスト4"];
+  List<FolderModel> _folderItems = [];
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  // }
+
+  // @override
+  // void dispose() {
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.green,
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () => {},
-        ),
-        actions: [
-          IconButton(
-              icon: Icon(_editFlag ? Icons.done : Icons.edit_rounded),
-              onPressed: () {
-                _switchEditMode();
-              }),
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () async {
-              String title = await showInputTitleDialog(context: context);
-              if (title != "") {
-                setState(() {
-                  folderList.add(title);
-                });
-              }
-            },
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => FolderListModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Colors.green,
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => {},
           ),
-        ],
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: folderList.length,
-        itemBuilder: (context, index) =>
-            _buildListTileView(folderList[index], index),
+          actions: [
+            IconButton(
+                icon: Icon(_editFlag ? Icons.done : Icons.edit_rounded),
+                onPressed: () {
+                  _switchEditMode();
+                }),
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () async {
+                String title = await showInputTitleDialog(context: context);
+                if (title != "") {
+                  setState(() {
+                    _folderItems.add(FolderModel(title, ''));
+                  });
+                }
+              },
+            ),
+          ],
+        ),
+        body: Consumer<FolderListModel>(builder: (context, folder, _) {
+          _folderItems = folder.items;
+          return ReorderableListView(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+            buildDefaultDragHandles: false,
+            children: <Widget>[
+              for (int index = 0; index < _folderItems.length; index++)
+                _buildListTileView(_folderItems[index].title, index)
+            ],
+            onReorder: (int oldIndex, int newIndex) {
+              setState(() {
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final FolderModel item = _folderItems.removeAt(oldIndex);
+                _folderItems.insert(newIndex, item);
+              });
+            },
+          );
+        }),
       ),
     );
   }
 
   Widget _buildListTileView(String text, int index) {
     return Card(
+        key: Key('$index'),
         child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        ListTile(
-          title: Text(text),
-          subtitle: Text(index.toString()),
-          trailing: Visibility(
-            visible: _editFlag,
-            child: Wrap(
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.edit),
-                  onPressed: () async {
-                    String title = await showInputTitleDialog(
-                        context: context, title: text);
-                    if (title != "") {
-                      setState(() {
-                        folderList[index] = title;
-                      });
-                    }
-                  },
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              title: Text(text),
+              subtitle: Text(index.toString()),
+              trailing: Visibility(
+                visible: _editFlag,
+                child: Wrap(
+                  children: <Widget>[
+                    ReorderableDragStartListener(
+                      index: index,
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Icon(Icons.drag_handle_rounded),
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () async {
+                        String title = await showInputTitleDialog(
+                            context: context, title: text);
+                        if (title != "") {
+                          setState(() {
+                            _folderItems[index] = FolderModel(title, '');
+                          });
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () async {
+                        if (await confirm(
+                          context,
+                          title: const Text('Confirm'),
+                          content: const Text('Would you like to remove?'),
+                          textOK: const Text('Yes'),
+                          textCancel: const Text('No'),
+                        )) {
+                          setState(() {
+                            _folderItems.removeAt(index);
+                            Fluttertoast.showToast(msg: "done!");
+                          });
+                        }
+                      },
+                    ),
+                  ],
                 ),
-                IconButton(
-                  icon: const Icon(Icons.delete),
-                  onPressed: () async {
-                    if (await confirm(
-                      context,
-                      title: const Text('Confirm'),
-                      content: const Text('Would you like to remove?'),
-                      textOK: const Text('Yes'),
-                      textCancel: const Text('No'),
-                    )) {
-                      setState(() {
-                        folderList.removeAt(index);
-                        Fluttertoast.showToast(msg: "done!");
-                      });
-                    }
-                  },
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    ));
+              ),
+            )
+          ],
+        ));
   }
 }
