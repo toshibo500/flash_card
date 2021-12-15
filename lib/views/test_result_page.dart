e5690c9b3d2d9f4094546019853339443e3c0afd
toshibo500 <walking.complex@gmail.com>
2021-12-15 22:58:07 +0900

    結果ページ結果一覧完成

--- lib/views/test_result_page.dart
+++ lib/views/test_result_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:flash_card/viewmodels/test_result_viewmodel.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -37,6 +38,25 @@ class _TestResultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var _testResultViweModel = Provider.of<TestResultViewModel>(context);
+
+    String _getDifferenceInSec(DateTime from, DateTime? to,
+        [bool addUnit = true]) {
+      String unit = addUnit ? L10n.of(context)!.sec : '';
+      return to != null ? '${to.difference(from).inSeconds}$unit' : 'N/A';
+    }
+
+    String _getTestingTime(TestResultViewModel model) {
+      DateTime from = model.test.startedAt;
+      DateTime? to = model.test.endedAt;
+      return _getDifferenceInSec(from, to, false);
+    }
+
+    String _getSccuracyRate(int noOfCorrect, int noOfQuestion,
+        [bool addUnit = true]) {
+      String rate = '${(noOfCorrect / noOfQuestion * 100).round()}';
+      return rate + (addUnit ? '%' : '');
+    }
+
     // 結果表示
     Widget _result = Column(
       mainAxisAlignment: MainAxisAlignment.start,
@@ -65,7 +85,10 @@ class _TestResultPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Text(
-                      '${_testResultViweModel.test.numberOfCorrectAnswers / _testResultViweModel.test.numberOfQuestions * 100}',
+                      _getSccuracyRate(
+                          _testResultViweModel.test.numberOfCorrectAnswers,
+                          _testResultViweModel.test.numberOfQuestions,
+                          false),
                       style: scoreLTextStyle,
                     ),
                     const Text(
@@ -105,27 +128,75 @@ class _TestResultPage extends StatelessWidget {
         ),
       ],
     );
-    // 結果表示一覧
+
+    // DataCell作成
+    DataCell _getDataCell(String txt,
+        [AlignmentGeometry align = Alignment.centerRight]) {
+      return DataCell(Container(alignment: align, child: Text(txt)));
+    }
+
+    // 結果表示リスト
+    List<DataRow> _resultRows = [];
+    for (var item in _testResultViweModel.testList) {
+      String title = '${item.numberOfCorrectAnswers}/${item.numberOfQuestions}';
+      String startAt = DateFormat('M/d HH:mm').format(item.startedAt);
+      String accuracyRate =
+          _getSccuracyRate(item.numberOfCorrectAnswers, item.numberOfQuestions);
+      String duration = _getDifferenceInSec(item.startedAt, item.endedAt!);
+      _resultRows.add(DataRow(cells: <DataCell>[
+        _getDataCell(startAt),
+        _getDataCell(title, Alignment.center),
+        _getDataCell(accuracyRate),
+        _getDataCell(duration),
+      ]));
+    }
+    TextStyle _dataTableColumnStyle =
+        const TextStyle(fontStyle: FontStyle.italic, fontSize: 12);
+    // 結果一覧テーブル
+    Widget _resultTable = DataTable(
+      horizontalMargin: 10,
+      columnSpacing: 35,
+      headingRowHeight: 30,
+      dataRowHeight: 45,
+      columns: [
+        DataColumn(
+          label: Text(
+            L10n.of(context)!.dateTime,
+            style: _dataTableColumnStyle,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            L10n.of(context)!.numberOfCorrect +
+                '/' +
+                L10n.of(context)!.numberOfQuestion,
+            style: _dataTableColumnStyle,
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            L10n.of(context)!.accuracyRate,
+            style: _dataTableColumnStyle,
+          ),
+        ),
+        DataColumn(
+          label: Text(
+            L10n.of(context)!.duration,
+            style: _dataTableColumnStyle,
+          ),
+        ),
+      ],
+      rows: _resultRows,
+    );
+
+    // 検索結果一覧
     Widget _resultList = Column(
       mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Text(L10n.of(context)!.resultList),
-        Container(
-          height: 400,
-          child: ListView.builder(
-            itemCount: _testResultViweModel.testList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Text(_testResultViweModel
-                      .testList[index].numberOfCorrectAnswers
-                      .toString() +
-                  '/' +
-                  _testResultViweModel.testList[index].numberOfQuestions
-                      .toString());
-            },
-          ),
-        )
-      ],
+      children: [Text(L10n.of(context)!.resultList), _resultTable],
     );
+
+    // Scaffold
     return Scaffold(
         appBar: AppBar(
           title: Text(_testResultViweModel.book.title),
@@ -138,11 +209,5 @@ class _TestResultPage extends StatelessWidget {
           ),
         ),
         body: Column(children: [_result, _resultList]));
-  }
-
-  String _getTestingTime(TestResultViewModel model) {
-    DateTime from = model.test.startedAt;
-    DateTime? to = model.test.endedAt;
-    return to != null ? '${to.difference(from).inSeconds}' : '';
   }
 }
